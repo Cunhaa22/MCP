64b0c6f6c2348333c4f2799dcf7bcba79f206c1f
Cunhaa22 <135064123+Cunhaa22@users.noreply.github.com>
2025-10-06 02:49:19 +0100

    Create __init__.py

---
+++ cst_python_api/analysis/__init__.py
@@ -0,0 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .farfield import get_gain_at_freq, get_axial_ratio_at_freq
+
+__all__ = ["get_gain_at_freq", "get_axial_ratio_at_freq"]
